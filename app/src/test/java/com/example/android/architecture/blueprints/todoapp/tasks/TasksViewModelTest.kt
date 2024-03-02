c2ea8891656c267ffc15a6ee5420bea49659bde8
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2024-03-01 23:17:48 -0300

    - Configurando a viewmodel test com o android x test e escrevendo assertions para o livedata. - Adicionado novas bibliotecas de teste.

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -0,0 +1,49 @@
+package com.example.android.architecture.blueprints.todoapp.tasks
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+
+@Config(sdk = [30])
+@RunWith(AndroidJUnit4::class)
+class TasksViewModelTest {
+    private lateinit var tasksViewModel: TasksViewModel
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setupViewModel() {
+        tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+    }
+
+    @Test
+    fun addNewTask_setsNewTaskEvent() {
+        // Quando adicionar uma tarefa
+        tasksViewModel.addNewTask()
+
+        // Então o novo evento da tarefa é acionado
+        val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
+        assertThat(value.getContentIfNotHandled(), (not(nullValue())))
+    }
+
+    @Test
+    fun setFilterAllTasks_tasksAddViewVisible() {
+        // Quando o tipo de filtro for  ALL_TASKS
+        tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
+
+        // Então verificar se a ação AddTask está visível
+        val value  = tasksViewModel.tasksAddViewVisible.getOrAwaitValue()
+        assertThat(value, equalTo(true))
+    }
+
+}
