0e8efd8448697d7e4a343e7379f1a3283d3eaa50
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2024-03-03 15:47:48 -0300

    - Iniciando um fragmento de um teste e criando um ServiceLocator.

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsViewModel.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsViewModel.kt
@@ -16,8 +16,10 @@
 
 package com.example.android.architecture.blueprints.todoapp.statistics
 
+import android.annotation.SuppressLint
 import android.app.Application
 import androidx.lifecycle.*
+import com.example.android.architecture.blueprints.todoapp.TodoApplication
 import com.example.android.architecture.blueprints.todoapp.data.Result
 import com.example.android.architecture.blueprints.todoapp.data.Result.Error
 import com.example.android.architecture.blueprints.todoapp.data.Result.Success
@@ -32,7 +34,7 @@ class StatisticsViewModel(application: Application) : AndroidViewModel(applicati
 
     // Note, for testing and architecture purposes, it's bad practice to construct the repository
     // here. We'll show you how to fix this during the codelab
-    private val tasksRepository = DefaultTasksRepository.getRepository(application)
+    private val tasksRepository = (application as TodoApplication).taskRepository
 
     private val tasks: LiveData<Result<List<Task>>> = tasksRepository.observeTasks()
     private val _dataLoading = MutableLiveData<Boolean>(false)
@@ -51,6 +53,7 @@ class StatisticsViewModel(application: Application) : AndroidViewModel(applicati
     val error: LiveData<Boolean> = tasks.map { it is Error }
     val empty: LiveData<Boolean> = tasks.map { (it as? Success)?.data.isNullOrEmpty() }
 
+    @SuppressLint("SuspiciousIndentation")
     fun refresh() {
         _dataLoading.value = true
             viewModelScope.launch {
